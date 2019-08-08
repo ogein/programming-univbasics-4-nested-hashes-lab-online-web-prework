32bf5c0895bc353e932230bf62bf03f62c1508f6
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-08 23:48:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
         }
      }
      
-  return programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 end
